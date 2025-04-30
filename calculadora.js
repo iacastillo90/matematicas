17c0447bbbe8db121eb0458af57bc6bca705d1f2
Iván Castillo <137194587+iacastillo90@users.noreply.github.com>
2025-04-29 21:26:20 -0400

    Agregar suma y resta a la Calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,19 +1,7 @@
 function sumar(a, b) {
     return a + b;
-}
+  }
   
-function restar(a, b) {
+  function restar(a, b) {
     return a - b;
-}
-
-sumar(5, 3);
-restar(10, 4);
-
-function multiplicar(a, b) {
-    return a * b;
-}
-  
-function dividir(a, b) {
-    if (b === 0) throw new Error("División por cero");
-    return a / b;
-}
+  }
